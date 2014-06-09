1dfbbd181a0acff9ba3b57a35281404238f26b1a
Grayson Wright <wright.grayson@gmail.com>
2014-06-09 13:25:47 -0400

    Fix gif loading error when not signed in

--- app/serializers/gif_post_serializer.rb
+++ app/serializers/gif_post_serializer.rb
@@ -8,6 +8,6 @@ class GifPostSerializer < ActiveModel::Serializer
   end
 
   def current_user_favorite_id
-    object.favorites.find_by(user_id: current_user.id).try :id
+    object.favorites.find_by(user_id: (current_user.id rescue nil)).try :id
   end
 end
